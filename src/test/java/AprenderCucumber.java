0c79991befc9c01820acd262f0effd4c181bcdfd
regis_alexandria <regisbarrosdealexandria@hotmail.com>
2024-06-26 21:01:56 -0300

    printegrando junit cucumber options cenario contador asserts e reaproveitamento de cenarios com regex

--- src/test/java/AprenderCucumber.java
+++ src/test/java/AprenderCucumber.java
@@ -1,22 +1,77 @@
+import com.sun.xml.internal.ws.spi.db.DatabindingException;
 import io.cucumber.java.pt.Dado;
 import io.cucumber.java.pt.Então;
 import io.cucumber.java.pt.Quando;
+import org.junit.Assert;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class AprenderCucumber {
 
     @Dado("que criei o arquivo corretamente")
-    public void que_criei_o_arquivo_corretamente() {
+    public void queCrieiOArquivoCorretamente() {
 
     }
+
     @Quando("executá-lo")
-    public void executá_lo() {
+    public void executáLo() {
 
     }
+
     @Então("a especificação deve finalizar com sucesso")
-    public void a_especificação_deve_finalizar_com_sucesso() {
+    public void aEspecificaçãoDeveFinalizarComSucesso() {
 
     }
 
+    private int contador = 0;
+
+    @Dado("que o valor do contador é {int}")
+    public void queOValorDoContadorÉ(Integer int1) {
+        contador = int1;
+    }
+
+    @Quando("eu incrementar em {int}")
+    public void euIncrementarEm(Integer int1) {
+        contador = contador + int1;
+    }
+
+    @Então("o valor do contador será {int}")
+    public void oValorDoContadorSerá(Integer int1) {
+        Assert.assertEquals((int) int1, (int) contador);
+    }
+
+    Date entrega = new Date();
+
+    @Dado("que a entrega é dia {int}\\/{int}\\/{int}")
+    public void que_a_entrega_é_dia(Integer dia, Integer mes, Integer ano) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.DAY_OF_MONTH, dia);
+        cal.set(Calendar.MONTH, mes - 1);
+        cal.set(Calendar.YEAR, ano);
+        entrega = cal.getTime();
+
+    }
+    @Quando("^a entrega atrasar em (\\d+) (dia|dias|mes|meses)$")
+    public void a_entrega_atrasar_em_dias(Integer int1, String tempo) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(entrega);
+        if(tempo.equals("dias")){
+            cal.add(Calendar.DAY_OF_MONTH, int1);
+        }
+        if(tempo.equals("meses")){
+            cal.add(Calendar.MONTH, int1);
+        }
+        entrega = cal.getTime();
+    }
+    @Então("^a entrega será efetuada em (\\d{2}\\/\\d{2}\\/\\d{4})$")
+    public void a_entrega_será_efetuada_em(String data) {
+        DateFormat format  = new SimpleDateFormat("dd/MM/yyyy");
+        String dataFormatada = format.format(entrega);
+        Assert.assertEquals(data, dataFormatada);
 
 
+    }
 }
